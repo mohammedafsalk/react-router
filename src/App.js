99c4b5b56588b2bd45ca59475f1a6040e8b6f0a2
mohammedafsalk <techafs456@gmail.com>
2023-05-29 11:09:29 +0530

    basicrouting and some styling added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes, Link,NavLink } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+    <header>
+      <nav>
+        <h1>Router</h1>
+        <NavLink to="/" >Home</NavLink>
+        <NavLink to="about" >About</NavLink>
+      </nav>
+    </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
